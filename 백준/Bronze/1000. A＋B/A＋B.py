0dc906700f29eb14072d3e707ac33f746a025ca7
Yelli915 <166230456+Yelli915@users.noreply.github.com>
2024-09-29 16:29:04 +0900

    Update A＋B.py

--- 백준/Bronze/1000. A＋B/A＋B.py
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -1,2 +1,9 @@
 a, b = map(int, input().split())
 print(a+b)
+
+1. input(), split() 먼저 처리하기 
+2. map(int, input.split()) 에 대하여
+- map() 함수는 리스트의 각 요소에 특정 함수를 적용하는 함수
+map(function, iterable)
+- function : 각 요소에 적용할 함수
+- iterable : 함수를 적용할 데이터 집
