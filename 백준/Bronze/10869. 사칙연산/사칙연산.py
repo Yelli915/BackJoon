2e7a29b1ba2a40bfe4360cbce08325b1494f8e04
Yelli915 <166230456+Yelli915@users.noreply.github.com>
2024-09-29 16:56:47 +0900

    [Bronze V] Title: 사칙연산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,7 @@
+A, B = map(int, input().split())
+addition = A + B
+subtraction = A - B
+multiplication = A * B
+division = A // B  
+modulus = A % B 
+print(addition, '\n', subtraction, '\n', multiplication, '\n', division, '\n', modulus, sep="")
