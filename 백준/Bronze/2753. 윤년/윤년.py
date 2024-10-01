9e8c2c49a99f002378a27e6fc221c9e1a2a7b418
Yelli915 <166230456+Yelli915@users.noreply.github.com>
2024-10-01 13:41:01 +0900

    Update 윤년.py

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -2,3 +2,6 @@ year = int(input())
 if (year%4==0 and year%100!=0) or (year%400==0) :
     print(1)
 else : print(0)
+
+# 논리 연산자 : and, or, not  ()
+# 배열이나 데이터프레임에서는 & | 사용 (비트 연산자, (앞에서 언급한 조건에서)논리 연산자)
