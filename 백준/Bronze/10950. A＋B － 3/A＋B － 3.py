6b9dfddd3ba05aab3239ab4663cbffeaa89fc6ef
Steven <166230456+Yelli915@users.noreply.github.com>
2024-10-23 12:04:14 +0900

    Update A＋B － 3.py

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -3,3 +3,8 @@ for number in range(n):
     a,b = map(int, input().split())
     if (a!='' and b!='') :
         print (a+b)
+
+n 줄의 순서쌍을 처리하겠다는 의미로 해석 
+
+사람들은 import sys 많이 했네 
+sys : 명령형 인자를 받아올 때 주로 사용
