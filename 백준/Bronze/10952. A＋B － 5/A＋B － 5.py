87faa04bd5bd5f07f06f17c17680fb2d6446535a
Steven <166230456+Yelli915@users.noreply.github.com>
2024-10-24 15:41:38 +0900

    Update A＋B － 5.py

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -9,3 +9,11 @@ while True:
 
 for index in data:
     print(index)
+
+while에서 break문 사용하기 (for보다는 while에 써야겠다)
+while True:
+    a, b = map(int, sys.stdin.readline().strip().split())
+    if a == 0 and b == 0:
+        break  # a와 b가 모두 0이면 반복을 종료
+    data.append(a + b)
+while True라는 무한 반복에서 조건이 달성하면 break - 끝나고 다음으로 넘어가겠다는 의미 (while 해당되는 것은 안하겠다)
