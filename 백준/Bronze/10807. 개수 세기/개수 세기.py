9c1fff0a53f8d6d44874be866b906dc1d81f95e7
Steven <166230456+Yelli915@users.noreply.github.com>
2024-10-28 21:11:42 +0900

    Update 개수 세기.py

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -9,3 +9,9 @@ for i in range(N):
         count += 1
 
 print(count)
+
+
+1. sys.stdin.readlind() -> 사용 결과는 문자열이므로 정수 변환이 필요함
+2. Java나 C언어와 달리 +=를 추가적으로 해야한다는 점 
+3. print(count)를 바로 할 수 있는 것은 정수형 변수의 나열로 만들었기에
+-- 배열이라면 len(count)였을텐데 
